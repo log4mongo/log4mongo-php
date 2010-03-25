761f759bf4c2b80ff8c6847c8167c2f3912acad0
Vladimir Gorej <gorej@mortality.sk>
2010-03-25 13:48:08 +0100

    First implementation of LoggerAppenderMongoDB

--- src/main/php/appenders/LoggerAppenderMongoDB.php
+++ src/main/php/appenders/LoggerAppenderMongoDB.php
@@ -1,5 +1,158 @@
 <?php
+/**
+ * The New BSD License
+ *
+ * Copyright (c) 2010, Vladimir Gorej
+ * All rights reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *  
+ *    * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *       
+ *     * The name of author may not be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @license New BSD License 
+ * @author char0n (Vladimir Gorej) <gorej@mortality.sk>  
+ * @package log4php
+ * @subpackage appenders
+ * @version 0.9a
+*/
+
 class LoggerAppenderMongoDB extends LoggerAppender {
     
+    protected static DEFAULT_MONGO_HOST      = 'localhost';
+    protected static DEFAULT_MONGO_PORT      = 27017;
+    protected static DEFAULT_DB_NAME         = 'log4php_mongodb';
+    protected static DEFAULT_COLLECTION_NAME = 'logs';    
+    
+    protected $hostname       = self::$DEFAULT_MONGO_HOST;
+    protected $port           = self::$DEFAULT_MONGO_PORT;
+    protected $dbName         = self::$DEFAULT_DB_NAME;
+    protected $collectionName = self::$DEFAULT_COLLECTION_NAME;
+    
+    protected $connection;
+    protected $collection;
+    
+    public function __construct($name = '') {
+        parent::__construct($name);
+        $this->requiresLayout = false;
+    }
+    
+    public function setHostname($hostname) {
+        $this->hostname = $hostname;
+    }
+    
+    public function getHostname() {
+        return $htis->hostname;
+    }
+    
+    public function setPort($port) {
+        $this->port = $port;
+    }
+    
+    public function getPort() {
+        return $this->port;
+    }
+    
+    public function setDatabaseName($dbName) {
+        $this->dbName = $dbName;
+    }
+    
+    public function getDatabaseName() {
+        return $this->dbName;
+    }
+    
+    public function setCollectionName($collectionName) {
+        $this->collectionName = $collectionName;
+    }
+    
+    public function getCollectionName() {
+        return $this->collectionName;
+    }
+    
+    /**
+     * Setup db connection.
+     * Based on defined options, this method connects to db defined in {@link $dbNmae}
+     * and creates a {@link $collection} 
+     * @return boolean true if all ok.
+     * @throws an Exception if the attempt to connect to the requested database fails.
+     */
+    public function activateOptions() {
+        try {
+            $this->connection = new Mongo(sprintf('%s:%d', $this->hostname, $this->port));
+            $db               = $this->connection->selectDB($this->dbName);
+            $this->collection = $db->selectCollection($this->collectionName);                        
+        } catch (MongoException $ex) {
+            $this->canAppend = false;
+            throw new LoggerException($ex);
+        }
+        
+        $this->canAppend = true;
+        return true;
+    }    
+    
+    /**
+     * Appends a new event to the mongo database.
+     * 
+     * @throws LoggerException  If the pattern conversion or the INSERT statement fails.
+     */
+    public function append(LoggerLoggingEvent $event) {    
+        if ($this->collection != null) {
+            $document = $this->LoggingEventToBSON($event);
+            $this->collection->insert($document);
+        }        
+    }
+    
+    /**
+     * Closes the connection to the logging database
+     */
+    public function close() {
+    	if($this->closed != true) {
+    	    $this->collection = null;
+    	    $this->connection->close();
+    	    $this->connection = null;
+        	$this->closed = true;
+    	}
+    }    
+    
+    public function __destruct() {
+        $this->close();
+    }
+    
+    /**
+     * @todo implement throwable info conversion, if implemented into log4php
+     */
+    protected loggingEventToBSON(LoggerLoggingEvent $event) {
+        $document = array(
+            'timestamp' => $event->getTimestamp(),
+            'level'     => $event->getLevel()->toString(),
+            'thread'    => $event->getThreadName(),
+            'message'   => $event->getMessage()
+        );
+        
+        if ($event->getLocationInformation() !== null) {
+            $document['fileName']   = $event->getLocationInformation()->getFileName();
+            $document['method']     = $event->getLocationInformation()->getMethodName();
+            $document['lineNumber'] = $event->getLocationInformation()->getLineNumber();
+            $document['className']  = $event->getLocationInformation()->getClassName();
+        }
+        
+        return $document;
+    }
 }
 ?>
